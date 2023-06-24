ee959f397e5ca6b187d174fac407611622c5a247
Lena Ji <99421857+lena-ji@users.noreply.github.com>
2023-06-24 14:03:23 -0400

    Update intquadricscheck.m

--- intquadricscheck.m
+++ intquadricscheck.m
@@ -5,19 +5,12 @@
 k:=Rationals();
 RP5<u,v,w,x,y,z>:= PolynomialRing(k, 6);
 
-Q1 := 1471*u^2 + 13372*u*v + 12461*u*w - 27530*u*x + 22448*u*y + 3650*u*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
-Q2 := -1294*u^2 - 11768*u*v - 10966*u*w + 24229*u*x - 19756*u*y - 3214*u*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
+Q1 := u*v + u*w - 4*v*w + 2*v*z + 2*w*z + x^2 - 2*x*z + y^2 - z^2;
+Q2 := u*v - u*w + u*y - 2*v^2 + 2*v*x - 2*w*y + 2*w*z + 2*x*z;
 P5:=Proj(RP5);
 X:= Scheme(P5, Q1) meet Scheme(P5, Q2);
 
 print "The intersection of quadrics is the", X;
-
 
 //////////////////////////////////////////////////////////////////////////////////
 // Check that X is smooth and contains a rational point
@@ -25,7 +18,7 @@ print "The intersection of quadrics is the", X;
 
 print "X is smooth?", IsNonsingular(X);
 
-point:=P5![2, -5/7, -4/7, -1/7, 1/7, 1];
+point:=P5![1, 0, 0, 0, 0, 0];
 point in X;
 
 print "So X contains the point", point;
@@ -60,51 +53,42 @@ print "The primes dividing the discriminant of Gamma are:", [primes[1][1],primes
 print "The real branch points of Gamma are";
 Roots(GammaBranch, RealField());
 
-
-
 //////////////////////////////////////////////////////////////////////////////////
 // Making an affine patch of the Fano variety of lines over F_2
 //////////////////////////////////////////////////////////////////////////////////
 
-// We use the first standard affine patch for Gr(2,6):
+// We use the affine patch for Gr(2,6) via the chart
 //
-// u  v  |  w  x  y   z
-// [1 0. | t1 t3 t5 t7 ]
-// [0 1  | t2 t4 t6 t8 ]
+// u  v  w  x  y  z
+// [t1 1 0 t3 t5 t7]
+// [t2 0 1 t4 t6 t8]
 
 prime:=2;
 
 k:=GF(prime);
 basering<u,v,w,x,y,z>:= PolynomialRing(k, 6);
 
-Q1 := 1471*u^2 + 13372*u*v + 12461*u*w - 27530*u*x + 22448*u*y + 3650*u*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
-Q2 := -1294*u^2 - 11768*u*v - 10966*u*w + 24229*u*x - 19756*u*y - 3214*u*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
+Q1 := u*v + u*w - 4*v*w + 2*v*z + 2*w*z + x^2 - 2*x*z + y^2 - z^2;
+Q2 := u*v - u*w + u*y - 2*v^2 + 2*v*x - 2*w*y + 2*w*z + 2*x*z;
 
 R<[t]> := PolynomialRing(k, 8);
-RP1<X, Y> := PolynomialRing(R, 2);
+RP1<r, s> := PolynomialRing(R, 2);
 
    
-phi := [X, Y, (t[1] * X + t[2] * Y ),
-       (t[3] * X + t[4] * Y ),
-        (t[5] * X + t[6] * Y ),
-       (t[7] * X + t[8] * Y )
+phi := [t[1] * r + t[2] * s, r, s,  
+       t[3] * r + t[4] * s, 
+       t[5] * r + t[6] * s, 
+       t[7] * r + t[8] * s
        ];
     
 eqns1 := Coefficients(Evaluate(Q1, phi));
 eqns2 := Coefficients(Evaluate(Q2, phi));
 ambient := Spec(R);
 Fanopatch := Scheme(ambient, eqns1) meet Scheme(ambient, eqns2);
-point1:=ambient![0, 0, 0, 1, 0, 0, 1, 1];
+point1:=ambient![1, 1, 0, 0, 1, 1, 0, 0];
 IsNonsingular(Fanopatch, point1);
 
 print "When p =", prime, "the Fano variety of lines contains the smooth point", point1;
-
 
 //////////////////////////////////////////////////////////////////////////////////
 // Making an affine patch of the Fano variety of lines over F_149743897
@@ -116,30 +100,25 @@ prime:=149743897;
 k:=GF(prime);
 basering<u,v,w,x,y,z>:= PolynomialRing(k, 6);
 
-Q1 := 1471*u^2 + 13372*u*v + 12461*u*w - 27530*u*x + 22448*u*y + 3650*u*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
-Q2 := -1294*u^2 - 11768*u*v - 10966*u*w + 24229*u*x - 19756*u*y - 3214*u*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
+Q1 := u*v + u*w - 4*v*w + 2*v*z + 2*w*z + x^2 - 2*x*z + y^2 - z^2;
+Q2 := u*v - u*w + u*y - 2*v^2 + 2*v*x - 2*w*y + 2*w*z + 2*x*z;
 
 R<[t]> := PolynomialRing(k, 8);
-RP1<X, Y> := PolynomialRing(R, 2);
+RP1<r, s> := PolynomialRing(R, 2);
 
    
-phi := [X, Y, (t[1] * X + t[2] * Y ),
-       (t[3] * X + t[4] * Y ),
-        (t[5] * X + t[6] * Y ),
-       (t[7] * X + t[8] * Y )
+phi := [t[1] * r + t[2] * s, r, s,  
+       t[3] * r + t[4] * s, 
+       t[5] * r + t[6] * s, 
+       t[7] * r + t[8] * s
        ];
     
 eqns1 := Coefficients(Evaluate(Q1, phi));
 eqns2 := Coefficients(Evaluate(Q2, phi));
 ambient := Spec(R);
 Fanopatch := Scheme(ambient, eqns1) meet Scheme(ambient, eqns2);
-point2:=ambient![23367516, 38034557, 52902585, 9110600, 36955379, 15930411,78325746,139150875];
+point2:=ambient![10276, 859210, 113976451, 113430900, 122036333, 94785567, 35411179,
+25838500];
 IsNonsingular(Fanopatch, point2);
 
 print "When p =", prime, "the Fano variety of lines contains the smooth point", point2;
@@ -151,14 +130,8 @@ print "When p =", prime, "the Fano variety of lines contains the smooth point", 
 k:=GF(149743897);
 R<u,v,w,x,y,z>:= PolynomialRing(k, 6);
 
-Q1 := 1471*u^2 + 13372*u*v + 12461*u*w - 27530*u*x + 22448*u*y + 3650*u*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
-Q2 := -1294*u^2 - 11768*u*v - 10966*u*w + 24229*u*x - 19756*u*y - 3214*u*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
+Q1 := u*v + u*w - 4*v*w + 2*v*z + 2*w*z + x^2 - 2*x*z + y^2 - z^2;
+Q2 := u*v - u*w + u*y - 2*v^2 + 2*v*x - 2*w*y + 2*w*z + 2*x*z;
 
 I := ideal<R | Q1, Q2>;
 X := Proj(quo<R | I>);
@@ -170,41 +143,24 @@ print "The dimension of Sing(X_149743897) is", Dimension(SingularSubscheme(X));
 print "Is Sing(X_149743897) irreducible?", IsIrreducible(SingularSubscheme(X));
 print "Sing(X_149743897) is the", PrimeComponents(SingularSubscheme(X));
 
-P:=SingularSubscheme(X)![87326150 , 44639405 , 42946036 , 20149960 , 14475939 , 1];
+P:=SingularSubscheme(X)![10925789 , 85737939 , 85378598 , 93099029 , 51694582 , 1];
 eqs := [Q1, Q2];
 J := JacobianMatrix(eqs);
-print "The Jacobian matrix at P := SingularSubscheme(X_149743897) has rank", Rank(Evaluate(J,[87326150 , 44639405 , 42946036 , 20149960 , 14475939 , 1]));
+print "The Jacobian matrix at P := SingularSubscheme(X_149743897) has rank", Rank(Evaluate(J,[10925789 , 85737939 , 85378598 , 93099029 , 51694582 , 1]));
 print "so P is non-conical";
 
 //////////////////////////////////////////////////////////////////////////////////
-// The reduction of X modulo p=2 is reducible
+// The reduction of X modulo p=2 is reducible and non-reduced
 //////////////////////////////////////////////////////////////////////////////////
-
 
 k:=GF(2);
 R<u,v,w,x,y,z>:= PolynomialRing(k, 6);
 
-Q1 := 1471*u^2 + 13372*u*v + 12461*u*w - 27530*u*x + 22448*u*y + 3650*u*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
-Q2 := -1294*u^2 - 11768*u*v - 10966*u*w + 24229*u*x - 19756*u*y - 3214*u*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
+Q1 := u*v + u*w - 4*v*w + 2*v*z + 2*w*z + x^2 - 2*x*z + y^2 - z^2;
+Q2 := u*v - u*w + u*y - 2*v^2 + 2*v*x - 2*w*y + 2*w*z + 2*x*z;
 
 I := ideal<R | Q1, Q2>;
 X := Proj(quo<R | I>);
 
 print "Is the reduction of X modulo 2 reduced?", IsReduced(X);
 print "Is the reduction of X modulo 2 irreducible?", IsIrreducible(X);
-
-print "The defining equations of X modulo 2 after the coordinate change u -> u + z are";
-1471*(u+z)^2 + 13372*(u+z)*v + 12461*(u+z)*w - 27530*(u+z)*x + 22448*(u+z)*y + 3650*(u+z)*z + 
-    30388*v^2 + 56632*v*w - 125128*v*x + 102064*v*y + 16580*v*z + 26386*w^2 -
-    116594*w*x + 95092*w*y + 15451*w*z + 128811*x^2 - 210156*x*y - 34132*x*z +
-    85760*y^2 + 27832*y*z + 2261*z^2;
--1294*(u+z)^2 - 11768*(u+z)*v - 10966*(u+z)*w + 24229*(u+z)*x - 19756*(u+z)*y - 3214*(u+z)*z -
-    26744*v^2 - 49844*v*w + 110120*v*x - 89792*v*y - 14600*v*z - 23225*w^2 +
-    102616*w*x - 83656*w*y - 13610*w*z - 113357*x^2 + 184884*x*y + 30051*x*z -
-    75468*y^2 - 24464*y*z - 1996*z^2;
